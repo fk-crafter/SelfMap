922b9a024ee76f9d8b5a904b1f61165fc611cec9
fk-crafter <fowad.kh@outlook.fr>
2026-09-12 16:06:55 +0200

    fix(ui): add favicon to root document links

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -31,6 +31,10 @@ export const Route = createRootRoute({
     ],
     links: [
       {
+        rel: 'icon',
+        href: '/favicon.ico',
+      },
+      {
         rel: 'stylesheet',
         href: appCss,
       },
