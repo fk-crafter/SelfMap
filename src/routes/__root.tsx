028ad3b1d1ff11778ffb90f9156450b8bd8e5f90
fk-crafter <fowad.kh@outlook.fr>
2026-09-12 16:14:50 +0200

    fix(ui): add favicon to root document links

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -35,6 +35,23 @@ export const Route = createRootRoute({
         href: '/favicon.ico?v=2',
       },
       {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      {
         rel: 'stylesheet',
         href: appCss,
       },
