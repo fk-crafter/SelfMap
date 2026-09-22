ae9a4f7971d2bfc04fcaef3dca0d1fea33a4b27c
fk-crafter <fowad.kh@outlook.fr>
2026-09-22 02:26:41 +0200

    chore(i18n): initialize i18next globally in root route

--- src/i18n.ts
+++ src/i18n.ts
@@ -5,18 +5,22 @@ import LanguageDetector from 'i18next-browser-languagedetector'
 import en from './locales/en.json'
 import fr from './locales/fr.json'
 
-i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    resources: {
-      en: { translation: en },
-      fr: { translation: fr },
-    },
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false,
-    },
-  })
+const isBrowser = typeof window !== 'undefined'
+
+if (isBrowser) {
+  i18n.use(LanguageDetector)
+}
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: en },
+    fr: { translation: fr },
+  },
+  fallbackLng: 'en',
+  lng: isBrowser ? undefined : 'en',
+  interpolation: {
+    escapeValue: false,
+  },
+})
 
 export default i18n
