8071b12ba58f73a5c71de74728fa30f2f743d197
fk-crafter <fowad.kh@outlook.fr>
2026-09-24 15:05:31 +0200

    fix(root): prevent input zoom on mobile by updating viewport meta in root file

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -46,7 +46,8 @@ export const Route = createRootRoute({
       },
       {
         name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
+        content:
+          'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0',
       },
       {
         title: 'SoulType',
